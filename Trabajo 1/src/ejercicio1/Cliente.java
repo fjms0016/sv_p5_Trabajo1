6011bade2d046be25eb70b162e605b1998ee0d77
fjms0016 <fjms0016@red.ujaen.es>
2015-10-08 12:55:06 +0200

    Inicio

---
+++ Trabajo 1/src/ejercicio1/Cliente.java
@@ -0,0 +1,10 @@
+package ejercicio1;
+
+public interface Cliente {
+	public void login();
+	public void envia(String men);
+	public String recibe();
+	public String descifrar(String men);
+	public void salir();
+
+}
---
+++ Trabajo 1/src/ejercicio1/Servicios.java
@@ -0,0 +1,17 @@
+package ejercicio1;
+
+public class Servicios {
+	
+	String inputData = null;
+	String outputData = "";
+	int estado = Mensajes.ID;
+}
+
+
+//Para contestar al cliente cuando introduce id OK o ERR crlf
+
+//Para contestar cuando solicita una aplicacion OK "" ID crlf
+
+//Para mandar la solucion a una cuenta OK""ID""numsec""datos crlf
+
+//Para salir OK""QUIT""Has salido.""CRLF
